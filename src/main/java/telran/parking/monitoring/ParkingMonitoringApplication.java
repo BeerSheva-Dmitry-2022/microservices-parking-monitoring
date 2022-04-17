20802927fcff70af5ca5fc4812cb6c221be0a53e
DimaTch <4dnik@mail.ru>
2022-04-17 18:10:42 +0300

    init project

---
+++ src/main/java/telran/parking/monitoring/ParkingMonitoringApplication.java
@@ -0,0 +1,13 @@
+package telran.parking.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingMonitoringApplication.class, args);
+    }
+
+}
